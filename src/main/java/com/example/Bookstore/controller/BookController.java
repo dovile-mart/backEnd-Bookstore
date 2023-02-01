7634593cc5957d1e2f092a11d8fda5fe524959ba
Dovile Mart <doviliu@gmail.com>
2023-02-01 12:54:15 +0200

    controllerin sisältöä siistitty

--- src/main/java/com/example/Bookstore/controller/BookController.java
+++ src/main/java/com/example/Bookstore/controller/BookController.java
@@ -1,20 +1,15 @@
 package com.example.Bookstore.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BookController {
 	
-	@GetMapping("/index")
-	public String indexPage() {
-		return "index";
-	//@RequestParam(name= "book", required=false) String book, Model model) {
-	//@RequestMapping(value = "/index", method = RequestMethod.GET)
+	@GetMapping("/index")		//http://localhost:8080/index
+	public String greetingFormBooks(Model model) {	
 	
-	//public String greetingFormBooks(Model model) {	
-	//ArrayList<Book> books = new ArrayList<>();
-	//	model.addAttribute("books", books);
-	//	return "books";
+		return "index";			//kutsuu index.html
 	}
 }
