f92144889e32f7a41a952690644a539494955df8
Dovile Mart <doviliu@gmail.com>
2023-03-15 21:00:36 +0200

    Controllerit testaukseen

--- src/test/java/com/example/Bookstore/BookstoreApplicationTests.java
+++ src/test/java/com/example/Bookstore/BookstoreApplicationTests.java
@@ -1,13 +1,26 @@
 package com.example.Bookstore;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.Bookstore.controller.BookController;
 
 @SpringBootTest
 class BookstoreApplicationTests {
 
+	
+	@Autowired
+	BookController bookController;
+	
+	@Autowired
+	BookController categoryController;
+	
 	@Test
-	void contextLoads() {
+	public void contextLoads() throws Exception {
+		assertThat(bookController).isNotNull();
+		assertThat(categoryController).isNotNull();
 	}
-
 }
