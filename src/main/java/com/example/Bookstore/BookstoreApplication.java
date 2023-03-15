d9d9451480ec830bfa4958a4aa184f87f1a14225
Dovile Mart <doviliu@gmail.com>
2023-03-15 20:52:01 +0200

    user ja admin käyttäjät lisätty

--- src/main/java/com/example/Bookstore/BookstoreApplication.java
+++ src/main/java/com/example/Bookstore/BookstoreApplication.java
@@ -8,6 +8,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import com.example.Bookstore.domain.AppUser;
+import com.example.Bookstore.domain.AppUserRepository;
 import com.example.Bookstore.domain.Book;
 import com.example.Bookstore.domain.BookRepository;
 import com.example.Bookstore.domain.Category;
@@ -18,7 +20,7 @@ public class BookstoreApplication implements CommandLineRunner {
 //public class BookstoreApplication {
 	
 	private static final Logger log = LoggerFactory.getLogger(BookstoreApplication.class);
-	// we need repository interfaces to put demo data to db
+	// we need repository interfaces to put demo data to db 
 
 	@Autowired
 	BookRepository bookRepository;
@@ -32,25 +34,27 @@ public class BookstoreApplication implements CommandLineRunner {
 
 	//demodatan syöttö
 	@Bean
-	public CommandLineRunner demo(BookRepository bookRepository, CategoryRepository categoryRepository) {
+	public CommandLineRunner demo(BookRepository bookRepository, CategoryRepository categoryRepository, AppUserRepository appusRepository) {
 	return (args) -> {
 	// Your code...add some demo data to db
 		//String title, String author, String isbn, int publicationYear, double price
-		log.info("Kategorioiden luonti");
+		log.info("Demokategorioiden luonti");
 		categoryRepository.save(new Category("Tietokirjat"));
 		categoryRepository.save(new Category("Lasten kirjat"));
 		categoryRepository.save(new Category("Kaunokirjat"));
 		
-		log.info("Demo tietokannan luonti");
-		log.info("Kirjojen luonti");
+		log.info("Demokirjojen luonti");
 		bookRepository.save(new Book("Huonetoveri", "Ruth Ware", "9789511450337", 2023, 27.95, categoryRepository.findByName("Kaunokirjat").get(0)));
 		bookRepository.save(new Book("Sota vai rauha", "Mihail Shishkin", "9789510491850", 2023, 33.95, categoryRepository.findByName("Tietokirjat").get(0)));
 		bookRepository.save(new Book("Pipsa ja unen lähtölaskenta", "Virve Lehväs", "9789511393924", 2021, 14.95, categoryRepository.findByName("Lasten kirjat").get(0)));
 		bookRepository.save(new Book("Neulottu Kalevala", "Jenna Kostet", "9789527468418", 2022, 34.95, categoryRepository.findByName("Tietokirjat").get(0)));
 		
+		AppUser user1 = new AppUser("user", "$2a$10$mR2WmGjLXN3t6v.cjNWY8upoHecRvcGjmq7slGq.9wwY83BD3yJkK", "USER");
+		AppUser user2 = new AppUser("admin", "$2a$10$jzx/BIs9sqsRfAaJOeGWeuqFbH/GG4Uxuz82ysgG5gn0ygew53GuW", "ADMIN");
+		appusRepository.save(user1);
+		appusRepository.save(user2);
 		
-		
-		log.info("Tulostetaan kirjat:");
+		log.info("Tulostetaan kaikki kirjat:");
 		for (Book book : bookRepository.findAll()) {
 			log.info(book.toString());
 		}
